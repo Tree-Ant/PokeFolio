c42c0f0404d9d0dbe00086ed65009bcc5a4ebaac
Tree Ant <stokes.finance@gmail.com>
2021-08-24 22:29:17 -0500

    "Update CSS, added filtering, and redesigned Log IN"

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 const BOXEL = document.querySelector(".BOXcontainer");
 const FORMEL = document.querySelector(".FORMcontainer");
 const ModalEL = document.querySelector("#ModalScreen");
-const ModalContEL = document.querySelector('#ModalContainer'); 
+const ModalContEL = document.querySelector('#ModalContainer');
+const FilterEL = document.querySelector('#FilterBar'); 
 const bodyEL = document.querySelector('body');
 
 const APIkey = 'ea74a294-34de-4f3a-9403-4af2f0328854'
@@ -38,9 +39,11 @@ modalToggle = () => {
 hoverToggle = () => {
     if(HOVERtoggler == 1) {
         BOXEL.classList.remove("no-hover");
+        FilterEL.style.display = "grid";
         HOVERtoggler = 0;
     } else {
-        BOXEL.classList.add("no-hover");
+        BOXEL.classList.add("no-hover");        
+        FilterEL.style.display = "none";
         HOVERtoggler = 1;       
     }
 }
@@ -101,6 +104,32 @@ SearchPokemonLog = (w1, w2, w3, o1) => {
             })
 
         }
+
+SearchPokemonLog2 = (w1, w2, w3, w4, w5, o1) => {
+    console.log("Search Language:  " + w1 + " is " + w2 + " " + w3 + " and is sorted by " + o1 )
+    console.log("CardType")
+    db.collection(AccountName).where(w1,w2,w3).where(w1,w4,w5).orderBy(o1).get().then((snapshot) => {
+        snapshot.docs.forEach((doc,i) => {
+            TotalPrice += doc.data().Price  
+            TotalCount ++ 
+    
+            setTimeout(() => {
+                
+                LogEL.innerHTML += 
+                '<div class="BOX ' + doc.data().CardType + 'TypeCardBox" id="' + doc.id + '" data-id="'+ doc.id + '">' +
+                '<div class="CardImageBox"><img class="PokemonCards" src="' + doc.data().CardIMG + '" alt="pokemoncard" onclick="CardView(this);"></div>'+
+                '<div class="CardTopTable"><table><tr><td>' + doc.data().CardSet + '</td><td>' + doc.data().CardNumber + '</td><td>' + doc.data().Rarity + '</td></tr></table></div>' +
+                '<div class="Price">$ ' + doc.data().Price.toFixed(2) + '</div><div class="DELETER" onclick="Deleter(this);">DELETE</div></div>'                 
+    
+            }, i * 100);
+        })
+            TOTEL = document.querySelector(".TotalSection");
+            
+            CardCountEL.innerHTML = TotalCount
+            CardPriceEL.innerHTML = "$ " + TotalPrice.toFixed(2)
+            })
+
+        }
         
 const newPokeform = document.querySelector('#addPokemonForm');
 
@@ -146,15 +175,22 @@ CardView = (element) => {
 const PokeFolioText = document.querySelector('#PokeFolioText');
 
 DefaultView = () => {
-    w1 = "PokeNumber" 
+    CreatePokemonLog();
+    PokeFolioText.innerHTML = AccountRealName + "'s PokeFolio"
+}
+
+PriceView = () => {
+    w1 = "Price" 
     w2 = ">"
-    w3 = 152
+    w3 = 10
     o1 = "PokeNumber"
-    CreatePokemonLog(w1, w2, w3, o1);
+    SearchPokemonLog(w1, w2, w3, o1);
     PokeFolioText.innerHTML = AccountRealName + "'s PokeFolio"
 }
 
 CardTypeSelectorEL = document.querySelector('#CardTypeSelector');
+GenSelectorEL = document.querySelector("#GenSelector");
+PriceSelectorEL = document.querySelector("#PriceSelector");
 
 CardTypeListener = () => {
     LogReset();
@@ -162,30 +198,114 @@ CardTypeListener = () => {
         console.log('Nothing to do')
         CreatePokemonLog();
     } else {
-        SearchByCardType();
+        w1 = "CardType" 
+        w2 = "=="
+        w3 = CardTypeSelectorEL.value ;
+        o1 = "CardType"
+        SearchPokemonLog(w1, w2, w3, o1);
     }    
 }
 
-SearchByCardType = () => { 
-    
-    console.log(CardTypeSelectorEL.value)
-
-    w1 = "CardType" 
-    w2 = "=="
-    w3 = CardTypeSelectorEL.value ;
-    o1 = "CardType"
-    SearchPokemonLog(w1, w2, w3, o1);
-    
+GenListener = () => {
+    LogReset();
+    if (GenSelectorEL.value == "None") {
+        console.log('Nothing to do')
+        CreatePokemonLog();
+    } else {
+        switch (GenSelectorEL.value) {
+            case 'Gen1':
+                w1 = "PokeNumber" 
+                w2 = ">"
+                w3 = 0;
+                w4 = "<"
+                w5 = 152
+                o1 = "PokeNumber"
+                SearchPokemonLog2(w1, w2, w3, w4, w5, o1);
+              break;
+              case 'Gen2':
+                w1 = "PokeNumber" 
+                w2 = ">"
+                w3 = 151;
+                w4 = "<"
+                w5 = 252
+                o1 = "PokeNumber"
+                SearchPokemonLog2(w1, w2, w3, w4, w5, o1);
+              break;
+              case 'Gen3':
+                w1 = "PokeNumber" 
+                w2 = ">"
+                w3 = 251;
+                w4 = "<"
+                w5 = 387
+                o1 = "PokeNumber"
+                SearchPokemonLog2(w1, w2, w3, w4, w5, o1);
+              break;
+              case 'Gen4':
+                w1 = "PokeNumber" 
+                w2 = ">"
+                w3 = 386;
+                w4 = "<"
+                w5 = 494
+                o1 = "PokeNumber"
+                SearchPokemonLog2(w1, w2, w3, w4, w5, o1);
+              break;
+              case 'Gen5':
+                w1 = "PokeNumber" 
+                w2 = ">"
+                w3 = 493;
+                w4 = "<"
+                w5 = 650
+                o1 = "PokeNumber"
+                SearchPokemonLog2(w1, w2, w3, w4, w5, o1);
+              break;
+              case 'Gen6':
+                w1 = "PokeNumber" 
+                w2 = ">"
+                w3 = 649;
+                w4 = "<"
+                w5 = 722
+                o1 = "PokeNumber"
+                SearchPokemonLog2(w1, w2, w3, w4, w5, o1);
+              break;
+              case 'Gen7':
+                w1 = "PokeNumber" 
+                w2 = ">"
+                w3 = 721;
+                w4 = "<"
+                w5 = 810
+                o1 = "PokeNumber"
+                SearchPokemonLog2(w1, w2, w3, w4, w5, o1);
+              break;
+              case 'Gen8':
+                w1 = "PokeNumber" 
+                w2 = ">"
+                w3 = 809;
+                w4 = "<"
+                w5 = 900
+                o1 = "PokeNumber"
+                SearchPokemonLog2(w1, w2, w3, w4, w5, o1);
+              break;
+            default:
+              console.log(`Sorry, we are out of ${expr}.`);
+          }
+    }    
 }
 
-// TestFunc = () => {
-// var newCityRef = db.collection("PCI3").doc();
+PriceListener = () => {
+    LogReset();
+    if (CardTypeSelectorEL.value == "None") {
+        console.log('Nothing to do')
+        CreatePokemonLog();
+    } else {
+        w1 = "CardType" 
+        w2 = "=="
+        w3 = CardTypeSelectorEL.value ;
+        o1 = "CardType"
+        SearchPokemonLog(w1, w2, w3, o1);
+    }    
+}
 
-// newCityRef.set(data);
-
-// }
-
-// TestFunc();
+// LogIN Program //
 
 const LogInform = document.querySelector('#LogInForm');
 const LogInContainer = document.querySelector('#LogInContainer');
@@ -194,6 +314,8 @@ const LogInContainer = document.querySelector('#LogInContainer');
 var LogInAttemptUSER = [];
 var LogInAttemptPW = [];
 var Authenticator = [];
+const LogInImage = document.querySelector(".LogInImage");
+const NavBar = document.querySelector(".NavBar");
 
 AuthenticateUSER = () => {
     db.collection("Accounts").where("UserName", '==' , LogInAttemptUSER).where("Password", '==' , LogInAttemptPW).get().then((snapshot) => {
@@ -206,23 +328,23 @@ AuthenticateUSER = () => {
     setTimeout(() => {        
         if (Authenticator == 1) {
             console.log('It worked')
-            LogInContainer.style.display = "none";            
+            LogInContainer.style.display = "none";
+            NavBar.style.display = "block"            
             DefaultView();
         } else {
             console.log('It didnt work')
+            LogInImage.classList.remove("SpinningImage");
             LogInform.reset();
         }
     }, 1000);
     
 } 
 
-
 LogInform.addEventListener('submit', (e) => {
     e.preventDefault();
-
+    LogInImage.classList.add("SpinningImage");
     LogInAttemptUSER = LogInform.UserName.value;
     LogInAttemptPW = LogInform.Password.value;
-    
     AuthenticateUSER();
     })
 
